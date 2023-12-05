1fcbf7147c9db60fc0173e748e216670fb3ddd92
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-05 23:02:43 +0530

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,15 @@
+//Call function
+
+#include<stdio.h>
+int x=10;
+int y;
+void office()
+{
+    printf("In Office -- Nusati coding\n");
+}
+void main()
+{
+    printf("Wake up\n");
+    office();
+    printf("Sleep\n");
+}
